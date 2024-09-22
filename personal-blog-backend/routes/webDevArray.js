09dfe892a95e9e47e5063ccb70eaca4909e970c0
NadineBSD <nadinebsd@outlook.com>
2024-09-22 14:26:30 +0100

    Changes to the backend folder

---
+++ personal-blog-backend/routes/webDevArray.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    host: "localhost", // Update this later for production
+    user: 'root',
+    password: '',
+    database: 'personal_blog'
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Database connection failed: ' + err.stack);
+        return;
+    }
+    console.log('Connected to database');
+});
+
+export default async function (req, res) {
+    const sql = "SELECT * FROM WEBDEVARRAY";
+    db.query(sql, (err, data) => {
+        if (err) return res.json(err);
+        return res.json(data);
+    });
+}
--- personal-blog-backend/server.js
+++ personal-blog-backend/server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express()
 app.use(cors())
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: 'root',
-    password: '',
-    database: 'personal_blog'
-})
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
 
 db.connect((err) => {
     if (err) {
@@ -20,33 +21,33 @@ db.connect((err) => {
     console.log('Connected to database');
 });
 
-app.get('/', (re, res) => {
-    return res.json("From Backend Side");
-})
+// app.get('/', (re, res) => {
+//     return res.json("From Backend Side");
+// })
 
-app.get('/designArray', (req, res) => {
-    const sql = "SELECT * FROM DESIGNARRAY"
-    db.query(sql, (err, data) => {
-        if (err) return res.json(err);
-        return res.json(data);
-    })
-})
+// app.get('/designArray', (req, res) => {
+//     const sql = "SELECT * FROM DESIGNARRAY"
+//     db.query(sql, (err, data) => {
+//         if (err) return res.json(err);
+//         return res.json(data);
+//     })
+// })
 
-app.get('/webDevArray', (req, res) => {
-    const sql = "SELECT * FROM WEBDEVARRAY"
-    db.query(sql, (err, data) => {
-        if (err) return res.json(err);
-        return res.json(data);
-    })
-})
+// app.get('/webDevArray', (req, res) => {
+//     const sql = "SELECT * FROM WEBDEVARRAY"
+//     db.query(sql, (err, data) => {
+//         if (err) return res.json(err);
+//         return res.json(data);
+//     })
+// })
 
-app.get('/mobileDevArray', (req, res) => {
-    const sql = "SELECT * FROM MOBILEDEVARRAY"
-    db.query(sql, (err, data) => {
-        if (err) return res.json(err);
-        return res.json(data);
-    })
-})
+// app.get('/mobileDevArray', (req, res) => {
+//     const sql = "SELECT * FROM MOBILEDEVARRAY"
+//     db.query(sql, (err, data) => {
+//         if (err) return res.json(err);
+//         return res.json(data);
+//     })
+// })
 
 app.listen(8081, () => {
     console.log("listening");
