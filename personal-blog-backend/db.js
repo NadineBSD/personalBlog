ace4bc09fdbdd70728461f89d96bd34493b5b1ed
NadineBSD <nadinebsd@outlook.com>
2024-09-22 15:23:07 +0100

    Update db.js

--- personal-blog-backend/db.js
+++ personal-blog-backend/db.js
@@ -1,12 +1,20 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
+// const db = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME,
+// });
+
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    host: "bvlrvu7kqrejwuqms85n-mysql.services.clever-cloud.com",
+    user: 'ucbzu7ueu6mqloog',
+    password: 'p8ZMio3C4lbCO5mYswTs',
+    database: 'bvlrvu7kqrejwuqms85n'
 });
+
 
 db.connect((err) => {
     if (err) {
