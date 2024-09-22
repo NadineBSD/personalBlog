788421df50ee611825cebff272b99d0b8d00b7d8
NadineBSD <nadinebsd@outlook.com>
2024-09-22 17:09:41 +0100

    Subtle changes in the routes

--- personal-blog-backend/routes/mobileDevArray.js
+++ personal-blog-backend/routes/mobileDevArray.js
@@ -1,7 +1,7 @@
 const db = require('../db'); // Import centralized DB connection
 
 module.exports = async function (req, res) {
-    const sql = "SELECT * FROM MOBILEDEVARRAY";
+    const sql = "SELECT * FROM mobileDevArray";
     db.query(sql, (err, data) => {
         if (err) return res.status(500).json(err);
         return res.json(data);
--- personal-blog-backend/routes/webDevArray.js
+++ personal-blog-backend/routes/webDevArray.js
@@ -1,7 +1,7 @@
 const db = require('../db'); // Import centralized DB connection
 
 module.exports = async function (req, res) {
-    const sql = "SELECT * FROM WEBDEVARRAY";
+    const sql = "SELECT * FROM webDevArray";
     db.query(sql, (err, data) => {
         if (err) return res.status(500).json(err);
         return res.json(data);
